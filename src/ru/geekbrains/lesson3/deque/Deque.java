0253b5e10fdef2a56d6ccd98cb8b170aee25bb15
Airt <alex.borichev@gmail.com>
2021-06-01 03:28:55 +0300

    Homework 2

---
+++ src/ru/geekbrains/lesson3/deque/Deque.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson3.deque;
+
+import lesson3.queue.Queue;
+
+public interface Deque<E> extends Queue<E> {
+    boolean insertLeft(E value);
+    boolean insertRight(E value);
+    E removeLeft();
+    E removeRight();
+}
